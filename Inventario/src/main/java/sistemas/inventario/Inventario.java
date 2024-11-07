87e77e7bf4f53111808d95c82f44694f9d5dd67b
Profesor Nur <profesornur2024@gmail.com>
2024-11-07 08:02:55 -0400

    Add files via upload

---
+++ Inventario/src/main/java/sistemas/inventario/Inventario.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sistemas.inventario;
+
+import GUI.Productos;
+
+/**
+ *
+ * @author user
+ */
+public class Inventario {
+
+    public static void main(String[] args) {
+  
+        /* Create and display the form */
+        Productos _productos = new Productos();
+        _productos.setVisible(true);
+  
+ 
+
+    }
+}
---
+++ PrimeraPantalla/src/main/java/com/mycompany/primerapantalla/PrimeraPantalla.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primerapantalla;
+
+/**
+ *
+ * @author user
+ */
+public class PrimeraPantalla {
+
+    public static void main(String[] args) {
+        Cliente formulario = new Cliente();
+        formulario.show();
+    }
+}
